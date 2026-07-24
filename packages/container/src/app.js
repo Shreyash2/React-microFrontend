9e14425090b8c0b3576b3dcc4f9ac5d0c87dfdc8
Shreyash2 <singh.shreyash656@gmail.com>
2026-07-25 00:37:10 +0530

    Initial commit

---
+++ packages/container/src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MarketingApp from "./components/MarketingApp";
+export default () => {
+  return (
+    <>
+      <h1> Container !</h1>
+      <hr />
+      <MarketingApp />
+    </>
+  );
+};
