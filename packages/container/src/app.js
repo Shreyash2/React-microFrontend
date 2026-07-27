85d44beae8317aa1364055d51622ca4d58758684
Shreyash2 <singh.shreyash656@gmail.com>
2026-07-28 00:48:26 +0530

    Routing and Css Fix

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -1,12 +1,26 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MarketingApp from "./components/MarketingApp";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
+
 export default () => {
   return (
-    <>
-      <h1> Container !</h1>
-      <hr />
-      <MarketingApp />
-    </>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
